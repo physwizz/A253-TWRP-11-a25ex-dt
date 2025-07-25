e5c7cc9049e8ed2da096aeb6a18ed6275eb4f54a
physwizz <pollockg86@gmail.com>
2025-07-25 20:05:05 +1000

    first

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a25ex.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a25ex-user \
+    twrp_a25ex-userdebug \
+    twrp_a25ex-eng
---
+++ twrp_a25ex.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+
+# Inherit from a25ex device
+$(call inherit-product, device/samsung/a25ex/device.mk)
+
+PRODUCT_DEVICE := a25ex
+PRODUCT_NAME := twrp_a25ex
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A253Q
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a25exjpn-user 13 TP1A.220624.014 A253QOPU1AYC1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a25exjpn/a25ex:13/TP1A.220624.014/A253QOPU1AYC1:user/release-keys
